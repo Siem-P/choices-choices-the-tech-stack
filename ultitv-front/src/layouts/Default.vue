5bcc1a4099ff3beb210dfa6c2ff9fbbe942461a2
Siem-P <64197688+Siem-P@users.noreply.github.com>
2023-10-20 14:49:07 +0200

    Update

--- ultitv-front/src/layouts/Default.vue
+++ ultitv-front/src/layouts/Default.vue
@@ -1,3 +1,18 @@
+
+<script>
+const data = fetch("http://localhost:8055/items/articles")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+console.log(data);
+</script>
+
+
 <template>
   <div class="layout">
     <header class="header">
@@ -9,7 +24,7 @@
         <g-link class="nav__link" to="/about/">About</g-link>
       </nav>
     </header>
-    <slot/>
+    <slot />
   </div>
 </template>
 
@@ -23,9 +38,10 @@ query {
 
 <style>
 body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
+  font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   line-height: 1.5;
 }
 
---
+++ ultitv-front/src/views/Article.vue
@@ -0,0 +1,12 @@
+<script setup>
+const data = fetch("http://localhost:8055/items/articles")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+console.log(data);
+</script>
